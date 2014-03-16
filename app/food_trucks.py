5ca807a2bec7c62423310b3e5155806717aff830
ireneyiu <ireyiu@gmail.com>
2014-03-15 21:35:20 -0700

    Add index.html page and render it in index route.

--- app/food_trucks.py
+++ app/food_trucks.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+from flask import Flask, render_template
 
-# create our little application :)
 app = Flask(__name__)
+app.debug = True
+app.config['API_JSON'] = 'http://data.sfgov.org/resource/rqzj-sfat.json';
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def index():
+	return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
